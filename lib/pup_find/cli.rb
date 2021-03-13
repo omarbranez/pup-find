1cd61608c35496c9a963a8413ce8618a007e4530
Omar Branez <omar.branez@gmail.com>
2021-03-13 11:20:54 -0500

    Working Scraper

--- lib/pup_find/cli.rb
+++ lib/pup_find/cli.rb
@@ -1,3 +1,15 @@
 class CLi
 
+    def test_one
+        puts "hello world"
+        self.test_two
+    end
+  
+    def test_two
+        puts "enter input"
+        Scraper.get_pup_hash
+        #input = gets.strip.to_i
+        #binding.pry
+    end
+
 end   
--- lib/pup_find/scraper.rb
+++ lib/pup_find/scraper.rb
@@ -1,3 +1,20 @@
 class Scraper
- 
+  #apikey not committed, for obvious reasons
+  def self.get_pup_hash
+    url = "https://api.rescuegroups.org/v5/public/animals/search/available/dogs/"
+    pup_response = HTTParty.post(url,
+      headers: {"Authorization" => {{APIKEY}}, "Content-Type" =>"application/vnd.api+json"},
+      body: "{\n    \"data\": {\n        \"filterRadius\": {\n            \"miles\": 50,\n            \"postalcode\": 11204\n        }\n    }\n}"
+      )
+  end
+
+  def color_hash
+    url = "https://api.rescuegroups.org/v5/public/animals/colors"
+    color_response = HTTParty.get(url,
+      headers: {"Authorization" => {{APIKEY}}, "Content-Type" =>"application/vnd.api+json"},
+      body: "{\n    \"data\": {\n        \"filterRadius\": {\n            \"miles\": 50,\n            \"postalcode\": 11204\n        }\n    }\n}"
+      )
+   end
+   
+
 end
